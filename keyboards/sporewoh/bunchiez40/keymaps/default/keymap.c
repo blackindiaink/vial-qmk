a19c21c9dfd971de9fc06efef6df2b8af00433ae
Sad Witch <132902087+blackindiaink@users.noreply.github.com>
2024-02-24 23:20:52 -0600

    trialkeymap.c

--- keyboards/sporewoh/bunchiez40/keymaps/default/keymap.c
+++ keyboards/sporewoh/bunchiez40/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,      KC_W,    KC_E,  KC_R,           KC_T,            KC_Y,  KC_U,              KC_I,    KC_O,    KC_P,
     KC_A,      KC_S,    KC_D,  KC_F,           KC_G,            KC_H,  KC_J,              KC_K,    KC_L,    KC_SEMICOLON,
     KC_Z,      KC_X,    KC_C,  KC_V,           KC_B,            KC_N,  KC_M,              KC_COMM, KC_DOT,  KC_SLSH,
-    KC_ESCAPE, KC_LGUI, KC_NO, LCTL_T(KC_TAB), LT(2, KC_SPACE), MO(1), KC_BACKSPACE, KC_LEFT_SHIFT,   KC_LALT, KC_ENTER),
+    KC_ESCAPE, KC_LGUI, KC_NO, LCTL_T(KC_TAB), LT(2, KC_Enter), MO(1), KC_SPACE, KC_LEFT_SHIFT,   KC_LALT, KC_Quote),
   [1] = LAYOUT_ortho_4x10(
     KC_1,      KC_2,     KC_3,     KC_4,        KC_5,        KC_6,    KC_7,    KC_8,      KC_9,     KC_0,
     KC_GRAVE,  KC_MINUS, KC_EQUAL, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_LEFT, KC_DOWN, KC_UP,     KC_RIGHT, KC_QUOTE,
